8937922b9a5bab907d2c0d600db5a4871487073e
Niktux <nico.ln@gmail.com>
2017-09-29 15:58:36 +0200

    chore: move from PSR-0 to PSR-4

--- src/Configuration/Memory.php
+++ src/Configuration/Memory.php
@@ -8,21 +8,26 @@ class Memory extends AbstractConfiguration
 {
     private
         $values;
-    
+
     public function __construct(array $values)
     {
         parent::__construct();
-        
+
         $this->values = $values;
     }
-    
+
     public function exists($fqn)
     {
         return array_key_exists($fqn, $this->values);
     }
-    
+
     protected function getValue($fqn)
     {
-        return $this->values[$fqn];
+        if(isset($this->values[$fqn]))
+        {
+            return $this->values[$fqn];
+        }
+
+
     }
 }
